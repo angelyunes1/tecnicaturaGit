645a4d433cafe142a829419a01c31ca78547da05
Angel Yunes <angelyunes1@gmail.com>
2022-06-08 20:45:04 -0300

    ingresamos los archivos de las clases de java (2)

--- java/leccion1/Ejercicio7/src/Ejercicio7/Ejercicio7.java
+++ java/leccion1/Ejercicio7/src/Ejercicio7/Ejercicio7.java
@@ -6,15 +6,30 @@ import java.util.Scanner;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        // salario =1000 + 150X+x0.05
-       
-        System.out.println("Cuantos autos has vendido? :");
-        var x = Integer.parseInt(entrada.nextLine());
-        System.out.println("Cual es valor del monto total de los auto vendidos? :");
-        var y = Float.parseFloat(entrada.nextLine());
-        var salario = 1000 + (150*x) + (0.05*y);
-        System.out.println("salario = " + salario);
+            Scanner entrada = new Scanner(System.in);
+//            // salario =1000 + 150X+x0.05
+//
+//            System.out.println("Cuantos autos has vendido? :");
+//            var x = Integer.parseInt(entrada.nextLine());
+//            System.out.println("Cual es valor del monto total de los auto vendidos? :");
+//            var y = Float.parseFloat(entrada.nextLine());
+//            var salario = 1000 + (150*x) + (0.05*y);
+//            System.out.println("salario = " + salario);
+            final int salario = 1000;
+            int comision = 150, venta;
+            float salarioMensual, ventaCarro, porcVenta,totalPrecio;
+            System.out.println("Digite la cantidad de carros vendidos: ");
+            venta = Integer.parseInt(entrada.nextLine());
+            System.out.println("Digite el precio del carro: ");
+            ventaCarro = Float.parseFloat(entrada.nextLine());
+            
+            comision *= venta;
+            totalPrecio = ventaCarro * venta;
+            porcVenta = totalPrecio * 0.05f;
+            salarioMensual = salario + comision + porcVenta;
+            System.out.println("\n El salario mensual es: " + salarioMensual);
+                    
+            
         
         
     }
