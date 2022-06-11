cff07c5329fdfb2436c1601bb52371d759ab2688
Angel Yunes <angelyunes1@gmail.com>
2022-06-10 21:55:27 -0300

    commit java (3) anadi llecion 2 y condicionalejercicio1.2.3

---
+++ java/leccion1/condicionalEjercicio2/src/condicionalEjercicio2/condicionalEjercicio2.java
@@ -0,0 +1,29 @@
+
+package condicionalEjercicio2;
+
+import java.util.Scanner;
+
+public class condicionalEjercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in); 
+        System.out.println("Digite el numero de un mes : ");
+        var mes = Integer.parseInt(entrada.nextLine());
+        var estacion = "estacion desconocida";
+        switch(mes){
+            case 1: case 2: case 3:
+                estacion = "Verano";
+                break;
+            case 4: case 5: case 6:
+                estacion = "Otono";
+                break;
+            case 7: case 8: case 9:
+                estacion = "Invierno";
+                break;
+            case 10: case 11: case 12:
+                estacion = "Primavera";
+                break;
+        }
+        System.out.println("estacion = " + estacion);
+                
+    }
+}
