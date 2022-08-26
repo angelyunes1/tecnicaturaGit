4a8b2a73c5f13578c5db2b1f71011a4f7ebda214
Angel Yunes <angelyunes1@gmail.com>
2022-08-26 19:29:15 -0300

    ingresamos los ultimos ejercicios en phyton java y javaScritp

--- python/Leccion4/main.py
+++ python/Leccion4/main.py
@@ -1,4 +1,7 @@
 # lista = Ariel , Liliana , Natalia , Osvaldo
+#Colecciones en python
+
+# las listas es lo que se conoce en otros lenguajes como vectores o arreglos
 
 nombres = ["Naty", "Osvaldo", "Lily", "Ariel"]
 print(nombres)
@@ -31,6 +34,11 @@ print(len(nombres)) # le pasamos como parametro la lista
 
 #agregamos un elemento
 nombres.append("marcelo")
+nombres.append([1,2,3])
+nombres.append(True)
+nombres.append(10.45)
+nombres.append([4,5])
+nombres.append(7)
 print(nombres)
 
 #insertar un nuevo elemento en un indice especifico
@@ -85,3 +93,120 @@ print('\n',cocina)
 
 #del cocina    \\esto es para eliminar una tupla
 
+# TIPO SET
+planetas = {"marte","jupiter","venus"}
+print(len(planetas)) #usamos la funcion len = lenght significa largo
+#revisar si un elemento existe dentro de set
+print("marte" in planetas)
+print("jupiter" not in planetas)
+#Agregar un elemento
+planetas.add("tierra") #add es una funcion
+planetas.add("tierra")
+planetas.add("tierra") #No se pueden agregar elementos duplicados o repetidos en set
+print(planetas)
+#eliminar elementos, puede arrojar un error si el elemento no extiste
+planetas.remove("jupiter") # en esta funcion un mal ingreso u inexistencia de este nos arroja error
+print(planetas)
+planetas.discard("Tierra") #con esta funcion tambien eliminamos elementos pero en este caso si nos equivocamos, no se borra
+print(planetas) #pero no salta un error
+
+# Limpiar nuestro set
+planetas.clear()
+print(planetas)
+#eliminar set
+del planetas
+#print(planetas)
+
+
+
+# 'maradona' : 10 un diccionario esta compuesto por dos elementos
+# UNA LLAVE Y UN VALOR
+# dict(key,value)
+diccionario = {
+    "IDE": "Integrated Development Envioronment",
+    'POO': "Programacion orientada a objetos",
+    "SABD": 'Sistema de Administracion de base de datos'
+}
+#verificar la cantidad de elementos del diccionario
+print(len(diccionario))
+print(diccionario)
+
+#Acceder a un diccionario con la llave(key)
+print(diccionario["IDE"])
+
+#otra forma de recuperar un elemento
+print(diccionario.get('POO'))
+print(diccionario.get('SABD'))
+
+#Modificar elementos
+diccionario["IDE"] = "Entorno de desarrollo integrado"
+print(diccionario)
+
+#como recorrer elementos
+for termino in diccionario: #recorremos mostrando solo las llaves
+    print(termino)
+#Necesitamos  una funcion para recorrer un diccionario
+for termino, valor in diccionario.items():
+    print(termino,valor)
+#otra forma de acceder a un diccionario
+for termino in  diccionario.keys():
+    print(termino) #solo muestra las llaves
+
+for valor in diccionario.values(): #usamos una funcion  para acceder al valor
+    print(valor)
+
+#comprobar la existencia de un elemento
+print("IDE" in diccionario)
+print("ide" not in diccionario) #devuelve un booleano
+
+#Agregar un elemento
+diccionario['PK'] = 'Primare Key'
+print(diccionario) # no se pueden agregar dos llaves iguales, en el caso de ingresar dos veces el mismo solo se reescribe.
+
+#eliminar un elemento
+diccionario.pop('SABD')
+print(diccionario)
+
+#como vaciar un diccionario
+diccionario.clear()
+print(diccionario)
+
+#eliminar el diccionario
+del diccionario #el diccionario se borro
+
+#concatenamos listas
+lista1 = [1,2,3,1]
+lista2 = [4,5,6,1]
+lista3 = lista1 + lista2 #concatenamos
+print(lista3)
+
+lista3.extend([7,8,9,1]) #funcion para agregar varios elementos a una lista
+print(lista3)
+
+print(lista3.index(5)) # Funcion para ubicar en que indice esta el valor ingresado
+# print(lista3.index(0)) Esto daria un error por ser un elemento que no es parte de la lista
+
+#como saber cuantos elementos repetidos hay dentro de una lista
+print(lista3.count(1)) # cuenta cuantos valores iguales hay dentro de la lista
+
+#para poner nuestra lista al reves
+lista3.reverse()
+print(lista3)
+
+#Para que una lista se multiplique repitiendo sus elementos
+lista3 = lista3 * 2
+print(lista3)
+
+#metodos de ordenamientos
+lista3.sort() #ordena los elementos ascendentemente
+print(lista3)
+lista3.sort(reverse=True) #ordena los elementos descendentemente
+print(lista3)
+
+#repaso de tuplas
+tupla = (4,"hola",6.78,[1,2,78],4,'hola')
+print(tupla)
+
+print(4 in tupla) #accion booleana, su repuesta es de tipo boleana
+#lo que podemos  usar dentro de tuplas son: index,count, len
+# EN tuplas se puede convertir de tupla a lista y de lista a tupla
