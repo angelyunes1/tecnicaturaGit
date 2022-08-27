44aa80c0423fdc288b365d8ef44d3da6672477f8
Angel Yunes <angelyunes1@gmail.com>
2022-08-26 21:06:04 -0300

    ingresamos las clases de java con  los siclos while, do while y for

---
+++ java/leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -0,0 +1,28 @@
+
+package CicloWhile;
+
+
+public class EjercicioWhile01 {
+    public static void main(String[] args) {
+        var conteo = 0; // inferencia de tipos
+        while (conteo < 3){
+            System.out.println("conteo = " + conteo);
+            conteo++; // vamos aumentando en uno la variable
+        }    
+        
+        System.out.println(" ");
+        
+        var contador = 0;
+        do{
+            System.out.println("contador = " + contador);
+            contador ++;
+        }while(contador <= 7);
+        
+        System.out.println(" ");
+        
+        for(var contando = 0; contando < 7; contando++){
+            System.out.println("contando = " + contando);
+        }
+    }
+    
+}
