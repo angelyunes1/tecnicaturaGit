2fdca130f1fe05f614e261a09857b45746f07772
angelyunes1 <angelyunes1@gmail.com>
2022-09-23 20:30:30 -0300

    Paso de argumentos en java y operador this
    Clase de java sobre Argumentos y operador this

--- java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -12,5 +12,17 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
     }
+    //Metodo2
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return this.a + this.b;
+    }   
+    
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; // EL argumento "a" se asigna al atributo this.a
+        this.b = b; // En este caso "this" sirve para diferenciar el atributo "b verde" y el parametro "b negro"
+        //return a + b;
+        return this.sumarConRetorno();
+    }
     
 }
