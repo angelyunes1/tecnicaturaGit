280bec5776450cec50876e04e9eb4d42b58dc553
angelyunes1 <angelyunes1@gmail.com>
2022-09-27 22:47:12 -0300

    Agregamos leccion 1 y 2 de JavaScript
    Agregamos leccion 1 y 2 de JavaScript
    27/9/22

---
+++ Javascript/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,18 @@
+// Tipos de datos en javaScrip
+/*
+Comentarios en multiple lineas
+La sintaxis de comentarios es muy similar a Java
+*/
+
+var nombre = 'Ariel'; //tipo Str
+console.log(nombre);
+
+var numero = 3000;//tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Ariel",
+    apellido : "Bentacud",
+    telefono : "261214122"
+}
+console.log(objeto);
