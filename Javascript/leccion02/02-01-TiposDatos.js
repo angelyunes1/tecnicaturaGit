eb3849e091b76f12fba773411d553e08b3b31c99
angelyunes1 <angelyunes1@gmail.com>
2022-10-11 22:04:19 -0300

    leccion02 de JavaScript y similitudes de sintaxis con Java

--- Javascript/leccion02/02-01-TiposDatos.js
+++ Javascript/leccion02/02-01-TiposDatos.js
@@ -4,10 +4,16 @@ Comentarios en multiple lineas
 La sintaxis de comentarios es muy similar a Java
 */
 
-var nombre = 'Ariel'; //tipo Str
-console.log(nombre);
-
-var numero = 3000;//tipo numerico
+//tipo Str
+var nombre = 'Ariel'; 
+console.log(typeof nombre);
+//tipo numerico
+nombre = 7;
+console.log(typeof nombre)
+nombre = 12.5;
+console.log(typeof nombre)
+//tipo numerico
+var numero = 3000;
 console.log(numero);
 
 var objeto = {
@@ -16,3 +22,45 @@ var objeto = {
     telefono : "261214122"
 }
 console.log(objeto);
+//tipo de dato booleano
+var bandera = true;
+console.log(bandera);
+
+//Tipo de dato Funcion
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+// Tipo de dato symbol
+var simbolo = Symbol("Mi simbolo");
+console.log(simbolo);
+
+//Tipos de dato clase
+class Persona {
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(typeof Persona)
+
+//Tipo de dato undefined
+var x ; 
+console.log(x);
+
+x = undefined;
+console.log(typeof x);
+
+// null: significa ausencia de valor
+var y = null; // null no es un tipo de dato, pero su origen es de tipo object
+console.log(typeof y);
+
+//Tipo de dato array y Empty String
+var autos = ['citroen','audi','ford','bmw'];
+console.log(autos);
+console.log(typeof autos); //Preguntamos que tipo de dato es (object)
+
+var z = ''; 
+console.log(z) //esto se refiere a que es una cadena vacia (empty string)
+console.log(typeof z)
+
