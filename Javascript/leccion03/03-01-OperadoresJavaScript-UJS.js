d4d506b7cbf192cf1bdc2343960af32363378687
angelyunes1 <angelyunes1@gmail.com>
2022-10-25 22:36:36 -0300

    Clase 7 de JavaScript
    Clase 7  de java, ejercicio de numeros pares, mayor de edad, similitudes

---
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar numeros pares e impares 
+let parInpar = 10;
+if(parInpar % 2 ==0){
+    console.log("Es un numero par")
+}
+else{
+    console.log("el numero es impar")
+}
+
+//Ejercicio: es mayor de edad
+let edad = 18, adulto = 18;
+if(edad >=adulto){
+    console.log("Es mayor de edad")
+}
+else{
+    console.log("Es menor de edad")
+}
