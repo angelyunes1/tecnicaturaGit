29ff50f34ae4e44bcd77f1ac77871a28989db160
angelyunes1 <angelyunes1@gmail.com>
2022-11-09 19:12:04 -0300

    Clase 9 Alguno Operadores y Algunas Funciones
    Operador or: Ejercicio, Si el padre puede asistir al juego de su hijo
    Operador ternario: Ejercicio Par Impar
    Función Number: Ejercicio, es mayor de edad (convertir a String)
    Función isNaN: is not number

--- Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -25,3 +25,44 @@ if(dentroRango >=valMin && dentroRango <= valorMax){
 else{
     console.log('Esta fuera del rango establecido')
 }
+
+//Ejercicio:Si el padre puede ir a ver el juego del hijo
+let vacaiones = false,diaDescanso = false;
+if (vacaiones || diaDescanso){
+    console.log("el padre puede asistir al juego de su hijo")
+}
+else{
+    console.log("el padre no puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "verdadero":"falso";
+console.log(resultado2)
+
+let numero = 9;
+resultado2 =numero % 2 == 0 ? "Es un numero par":"es un numero impar"
+console.log(resultado2)
+
+//convertir string a numero
+let miNumero = "10";//es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero); //Esta es una funcion
+console.log(typeof edad2)
+
+//Funcion isNan
+if(isNaN(edad2)){//no es un numero = is not a Number (devuelve un resultado booleano)
+    console.log("Esta variable no contiene solo numeros")
+}
+else{
+     if (edad2 >=18){
+        console.log("Puede votar");
+    } 
+    else{
+        console.log("No puede votar");
+    }  
+}
+
+//Operador ternario
+let resultado3 = edad2 >= 18 ? "Puede votar":"No puede votar";
+console.log(resultado3);
+
