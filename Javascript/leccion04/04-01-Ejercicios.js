8d5bc9e5c77372f7c49907b02f1b44c3dd6c71c9
angelyunes1 <angelyunes1@gmail.com>
2022-11-15 21:58:47 -0300

    clase 10 java
    Clase 10 Hagamos ejercicios en JavaScript

--- Javascript/leccion04/04-01-Ejercicios.js
+++ Javascript/leccion04/04-01-Ejercicios.js
@@ -1,4 +1,68 @@
+//Ejercicio1:Calcular estacion de ano
+let mes =1;
+let estacion; //Undifined
+if (mes == 1 || mes ==2 || mes ==12 ){
+    estacion = "verano";
+}
+else if(mes == 3 || mes == 4 || mes ==5 ){
+    estacion = "Otono ";
+}
+else if(mes == 7 || mes == 6 || mes ==8 ){
+    estacion = " Invierno "
+}
+else if(mes == 10 || mes == 11 || mes == 9 ){
+    estacion = " Primavera ";
+}
+else{
+    estacion ="valor incorrecto";
+}
+console.log("El mes " +mes+ " se encuentra en la estacion: "+estacion)
 
+//Ejercicio2: Hora del dia 
+let hora =5 ;
+let mensaje;
+
+if(hora >= 6 && hora <= 11){
+    mensaje = "Buen dia";
+} 
+else if (hora >=12 && hora <= 16){
+    mensaje = "hora de la siesta";
+}
+else if(hora >=17 && hora <= 19){
+    mensaje= "buenas tardes";
+}
+else if(hora >=20 && hora <= 23){
+mensaje= "es de noche";
+
+}
+else if(hora >=0 && hora <= 5){
+    mensaje= "es de noche";
+    
+    }
+else{
+    mensaje= "valor incorrecto";
+}
+console.log(mensaje)
+
+
+//Estructura Switch(La sintaxis es igual a java)
+switch(mes){
+    case 1: case 2: case 12:
+        estacion = "verano";
+        break;
+    case 3: case 4: case 5:
+        estacion = "otono";
+        break;
+    case 6: case 7: case 8:
+        estacion = "invierno";
+        break;
+    case 9: case 10: case 11:
+        estacion = "primavera";
+        break;
+    default:
+        estacion ="valor incorrecto"
+}   
+console.log(estacion)
 
 
 //Ampliamos el uso de var; let; y const;
